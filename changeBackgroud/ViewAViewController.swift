3842fc8b3f7f5a40df608286351fff88e97990a5
BuiNgocSonxx04 <sonbeat1204@gmail.com>
2021-07-27 15:22:46 +0700

    thay doi mau viewcontroler

---
+++ changeBackgroud/ViewAViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewAViewController.swift
+//  changeBackgroud
+//
+//  Created by son on 7/27/21.
+//
+
+import UIKit
+
+class ViewAViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func viewRed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "viewcolor") as! ViewColorViewController
+        vc.view.backgroundColor = .red
+        navigationController?.pushViewController(vc,animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- changeBackgroud/ViewController.swift
+++ changeBackgroud/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextA(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "viewa") as! ViewAViewController
+        navigationController?.pushViewController(vc,animated: true)
+    }
+    
+    
+    @IBAction func nextB(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "viewb") as! ViewBViewController
+        navigationController?.pushViewController(vc,animated: true)
+    }
 }
 
